24f9638218bd20efbe5bb06442323c04d7ff38d1
Tina Oluwabukola <oluwabukolatina@gmail.com>
2021-02-26 18:18:51 +0100

    test: user can register

--- src/lib/modules/auth/utils/helper.ts
+++ src/lib/modules/auth/utils/helper.ts
@@ -1,12 +1,6 @@
-import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 const jwtHelper = {
-  createToken(id: number) {
-    return jwt.sign({ userId: id }, String(process.env.JWT_SECRET), {
-      expiresIn: '3600000',
-    });
-  },
   hashPassword(password: string) {
     return bcrypt.hashSync(password, 10);
   },
--- test/auth.test.ts
+++ test/auth.test.ts
@@ -1,21 +1,26 @@
 import request from 'supertest';
 import { expect } from 'chai';
+import * as faker from 'faker';
 import app from '../src/app';
 import 'chai/register-should';
 import { HTTP_CREATED } from '../src/lib/utils/status-codes/http-status-codes';
+import { AUTH_URL } from '../src/lib/utils/urls';
 
-const QUESTIONS_URL = '/api/v1/korapay/question';
 describe('questions /question', () => {
   it('should create a question', () => {
     request(app)
-      .post(`${QUESTIONS_URL}`)
-      .send({ title: 'dummy question', body: 'dummy body' })
+      .post(`${AUTH_URL}/register`)
+      .send({
+        email: faker.internet.email(),
+        password: faker.random.word(),
+      })
       .expect(HTTP_CREATED)
       .end((err: Error, res) => {
         expect(res.status).to.eql(201);
-        expect(res.body.message).to.eql('Created Question');
+        expect(res.body.message).to.eql('User Created');
         expect(res.body.status).to.eql(true);
         expect(res.body.should.have.property('data'));
+
       });
   });
 });
