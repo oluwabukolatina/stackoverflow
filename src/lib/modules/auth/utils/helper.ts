7b8dd25e1410a47194bc62f5bbfec4d757017e4a
Tina Oluwabukola <oluwabukolatina@gmail.com>
2021-02-26 23:40:17 +0100

    test: validation on register

--- src/lib/modules/auth/utils/helper.ts
+++ src/lib/modules/auth/utils/helper.ts
@@ -1,6 +1,12 @@
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 const jwtHelper = {
+  createToken(id: number) {
+    return jwt.sign({ userId: id }, String(process.env.JWT_SECRET), {
+      expiresIn: '3600000',
+    });
+  },
   hashPassword(password: string) {
     return bcrypt.hashSync(password, 10);
   },
--- test/auth.test.ts
+++ test/auth.test.ts
@@ -3,11 +3,14 @@ import { expect } from 'chai';
 import * as faker from 'faker';
 import app from '../src/app';
 import 'chai/register-should';
-import { HTTP_CREATED } from '../src/lib/utils/status-codes/http-status-codes';
+import {
+  HTTP_BAD_REQUEST,
+  HTTP_CREATED,
+} from '../src/lib/utils/status-codes/http-status-codes';
 import { AUTH_URL } from '../src/lib/utils/urls';
 
-describe('questions /question', () => {
-  it('should create a question', () => {
+describe('auth /auth', () => {
+  it('should create a user', () => {
     request(app)
       .post(`${AUTH_URL}/register`)
       .send({
@@ -16,11 +19,41 @@ describe('questions /question', () => {
       })
       .expect(HTTP_CREATED)
       .end((err: Error, res) => {
-        expect(res.status).to.eql(201);
+        expect(res.status).to.eql(HTTP_CREATED);
         expect(res.body.message).to.eql('User Created');
         expect(res.body.status).to.eql(true);
         expect(res.body.should.have.property('data'));
+      });
+  });
+  it('should not create a user if no email', () => {
+    request(app)
+      .post(`${AUTH_URL}/register`)
+      .send({
+        password: faker.random.word(),
+      })
+      .expect(HTTP_BAD_REQUEST)
+      .end((err: Error, res) => {
+        expect(res.status).to.eql(HTTP_BAD_REQUEST);
+        expect(res.body.message).to.eql('Unable to register');
+        expect(res.body.status).to.eql(false);
+        expect(res.body.should.have.property('error'));
+        expect(res.body.error[0].message).to.eql('email is required');
+      });
+  });
 
+  it('should not create a user if no password', () => {
+    request(app)
+      .post(`${AUTH_URL}/register`)
+      .send({
+        email: faker.internet.email(),
+      })
+      .expect(HTTP_BAD_REQUEST)
+      .end((err: Error, res) => {
+        expect(res.status).to.eql(HTTP_BAD_REQUEST);
+        expect(res.body.message).to.eql('Unable to register');
+        expect(res.body.status).to.eql(false);
+        expect(res.body.should.have.property('error'));
+        expect(res.body.error[0].message).to.eql('password is required');
       });
   });
 });
