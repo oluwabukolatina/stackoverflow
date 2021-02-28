953d83a2a0ecac53aba4b89ff84fceebf14d373a
Tina Oluwabukola <oluwabukolatina@gmail.com>
2021-02-28 21:37:56 +0100

    seeders

---
+++ database/config/config.ts
@@ -0,0 +1,35 @@
+
+require('dotenv').config();
+module.exports ={
+  development: {
+    username: 'root',
+    password: 'root',
+    database: 'stac4',
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
+
+//{
+//  ""development": {
+//    username: 'root',
+//    password: 'root',
+//    database: 'stac4',
+//    host: process.env.DB_HOST,
+//    dialect: 'mysql',
+//  },
+//};
