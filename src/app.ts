1969f954a35fe71883896e1f050f28f91760165d
Tina Oluwabukola <oluwabukolatina@gmail.com>
2021-03-01 15:22:36 +0100

    rm dummy

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import express from 'express';
 import * as winston from 'winston';
 import * as expressWinston from 'express-winston';
 import QuestionRoutes from './v1/lib/modules/question/route/question.route';
-import DummyRoutes from './v1/lib/modules/dummy/route/dummy.route';
 import AuthRoutes from './v1/lib/modules/auth/route/auth.route';
 import AnswerRoutes from './v1/lib/modules/answer/route/answer.route';
 import SubscriptionRoutes from './v1/lib/modules/subscriptions/route/subscription.route';
@@ -19,18 +18,16 @@ class App {
 
   public questionRoutes: QuestionRoutes = new QuestionRoutes();
 
-  public dummyRoutes: DummyRoutes = new DummyRoutes();
-
   public authRoutes: AuthRoutes = new AuthRoutes();
 
   public answerRoutes: AnswerRoutes = new AnswerRoutes();
+
   public subscriptionRoutes: SubscriptionRoutes = new SubscriptionRoutes();
 
   constructor() {
     this.app = express();
     this.config();
     this.questionRoutes.routes(this.app);
-    this.dummyRoutes.routes(this.app);
     this.authRoutes.routes(this.app);
     this.answerRoutes.routes(this.app);
     this.subscriptionRoutes.routes(this.app);
