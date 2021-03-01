ba9379eb8821bf0ae38ff3441f4d94c2e4b60e78
Tina Oluwabukola <oluwabukolatina@gmail.com>
2021-03-01 14:22:05 +0100

    fix tests

--- src/v1/lib/modules/question/test/question.test.ts
+++ src/v1/lib/modules/question/test/question.test.ts
@@ -1,11 +1,8 @@
 import request from 'supertest';
 import faker from 'faker';
-import app from '../src/app';
-import {
-  HTTP_CREATED,
-  HTTP_OK,
-} from '../src/v1/lib/utils/status-codes/http-status-codes';
-import { AUTH_URL, QUESTIONS_URL } from '../src/v1/lib/utils/urls';
+import app from '../../../../../app';
+import * as url from '../../../utils/urls';
+import * as statusCodes from '../../../utils/status-codes/http-status-codes';
 
 describe('questions /question', () => {
   let token = '';
@@ -15,32 +12,32 @@ describe('questions /question', () => {
       email: faker.internet.email(),
       password: faker.random.word(),
     };
-    const res = await request(app).post(`${AUTH_URL}/register`).send(data);
-    expect(res.status).toEqual(HTTP_CREATED);
+    const res = await request(app).post(`${url.AUTH_URL}/register`).send(data);
+    expect(res.status).toEqual(statusCodes.HTTP_CREATED);
     token = res.body.data.token;
   });
   beforeAll(async () => {
     const res = await request(app)
-      .post(`${QUESTIONS_URL}`)
+      .post(`${url.QUESTIONS_URL}`)
       .send({ title: faker.lorem.sentences(1), body: faker.lorem.paragraph(3) })
       .set('X-Auth-Token', token);
-    expect(res.status).toEqual(HTTP_CREATED);
+    expect(res.status).toEqual(statusCodes.HTTP_CREATED);
     questionId = res.body.data.question.id;
   });
   it('should create a question', async () => {
     const res = await request(app)
-      .post(`${QUESTIONS_URL}`)
+      .post(`${url.QUESTIONS_URL}`)
       .send({ title: faker.lorem.sentences(1), body: faker.lorem.paragraph(3) })
       .set('X-Auth-Token', token);
-    expect(res.status).toEqual(HTTP_CREATED);
+    expect(res.status).toEqual(statusCodes.HTTP_CREATED);
     expect(res.body.message).toEqual('Created Question');
     expect(res.body.status).toEqual(true);
     expect(res.body).toHaveProperty('data');
     expect(res.body.data).toHaveProperty('question');
   });
   it('should get all questions', async () => {
-    const res = await request(app).get(`${QUESTIONS_URL}`);
-    expect(res.status).toEqual(HTTP_OK);
+    const res = await request(app).get(`${url.QUESTIONS_URL}`);
+    expect(res.status).toEqual(statusCodes.HTTP_OK);
     expect(res.body.message).toEqual('Fetched Questions');
     expect(res.body.status).toEqual(true);
     expect(res.body).toHaveProperty('data');
@@ -48,9 +45,9 @@ describe('questions /question', () => {
   });
   it('should upvote a question', async () => {
     const res = await request(app)
-      .put(`${QUESTIONS_URL}/upvote/${questionId}`)
+      .put(`${url.QUESTIONS_URL}/upvote/${questionId}`)
       .set('X-Auth-Token', token);
-    expect(res.status).toEqual(HTTP_OK);
+    expect(res.status).toEqual(statusCodes.HTTP_OK);
     expect(res.body.message).toEqual('Upvoted Question');
     expect(res.body.status).toEqual(true);
     expect(res.body).toHaveProperty('data');
@@ -58,9 +55,9 @@ describe('questions /question', () => {
   });
   it('should downvote a question', async () => {
     const res = await request(app)
-      .put(`${QUESTIONS_URL}/downvote/${questionId}`)
+      .put(`${url.QUESTIONS_URL}/downvote/${questionId}`)
       .set('X-Auth-Token', token);
-    expect(res.status).toEqual(HTTP_OK);
+    expect(res.status).toEqual(statusCodes.HTTP_OK);
     expect(res.body.message).toEqual('Downvoted Question');
     expect(res.body.status).toEqual(true);
     expect(res.body).toHaveProperty('data');
