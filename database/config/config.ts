7cf6033f80f06c057255711e02fb35d7dec40951
Tina Oluwabukola <oluwabukolatina@gmail.com>
2021-03-01 09:34:33 +0100

    prod db

--- database/config/config.ts
+++ database/config/config.ts
@@ -8,18 +8,11 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: 'mysql',
   },
-  test: {
+  production: {
     username: process.env.PROD_DB_USER,
     password: process.env.PROD_DB_PASSWORD,
     database: process.env.PROD_DB,
     host: process.env.PROD_DB_HOST,
-    dialect: 'mysql',
-  },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
     dialect: 'mysql',
   },
 };
